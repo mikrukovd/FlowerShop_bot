47df7182da19d9742782dd71fc99fa0205f7133a
mikrukovd <z123456689z@gmail.com>
2025-09-25 11:11:14 +0300

    Сделал начальный функционал бота

--- ptb/handlers/callback_handlers.py
+++ ptb/handlers/callback_handlers.py
@@ -1,8 +1,13 @@
 from . import states_bot
-from ptb.keyboards.keyboard import *
+from ptb.keyboards.keyboard import (
+    shade_menu_kb, price_kb, choose_flowers_kb, delivery_date_kb,
+    delivery_time_kb, confirm_order_kb, main_menu_kb, yes_no_kb,
+    remove_flower_kb, opd_kb
+)
 
 
 async def edit_message(query, text, reply_markup):
+    '''Редактор сообшения'''
     await query.edit_message_text(
         text=text,
         reply_markup=reply_markup
@@ -10,3 +15,187 @@ async def edit_message(query, text, reply_markup):
 
 
 async def handler_main_menu(update, context):
+    '''Обработчик главного меню'''
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "any_reason":
+        await edit_message(query, "Выбор события", None)
+        return states_bot.OTHER_EVENT
+
+    await edit_message(query, "Выбор оттенка", shade_menu_kb)
+    return states_bot.SHADE_MENU
+
+
+async def handler_shade_menu(update, context):
+    '''Обработчик меню оттенков'''
+    query = update.callback_query
+    await query.answer()
+
+    await edit_message(query, "Выбор бюджета", price_kb)
+    return states_bot.PRICE_MENU
+
+
+async def handler_price_menu(update, context):
+    '''Обработчик меню цен'''
+    query = update.callback_query
+    await query.answer()
+
+    # Заглушка для демонстрации букета
+    text = "Ваш букет:\nФото \nСостав: Розы, тюльпаны, лилии\nОписание: Красивый букет\nЦена: 1500 руб."
+    await edit_message(query, text, choose_flowers_kb)
+    return states_bot.FLOWERS
+
+
+async def handler_flowers(update, context):
+    '''Обработчик выбора букета'''
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "confirm_flowers":
+        text = "Хотите убрать какой-нибудь цветок из букета?"
+        await edit_message(query, text, yes_no_kb)
+        return states_bot.REMOVE_FLOWER
+
+    elif query.data == "another_flowers":
+        await edit_message(query, "Выбор бюджета", price_kb)
+        return states_bot.PRICE_MENU
+
+    elif query.data == "need_consult":
+        # TODO: Тут переход к сценарию консультации
+        await edit_message(query, "Консультация", main_menu_kb)
+        return states_bot.MAIN_MENU
+
+    return states_bot.FLOWERS
+
+
+async def handler_remove_flower(update, context):
+    '''Обработчик удаления цветка'''
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "yes":
+        text = "Какой цветок вы хотите убрать из букета?"
+        await edit_message(query, text, remove_flower_kb)
+        return states_bot.REMOVE_FLOWER
+
+    elif query.data == "no":
+        await edit_message(query, "Согласие с обработкой данных", opd_kb)
+        return states_bot.OPD
+
+    elif query.data.startswith("remove_"):
+        removed_flower = query.data.replace("remove_", "")
+
+        if removed_flower == "nothing":
+            text = "Цветок не удален. Переход к соглашению ОПД."
+        else:
+            text = f"Цветок '{removed_flower}' удален из букета."
+            context.user_data['removed_flower'] = removed_flower
+
+        await edit_message(query, text, opd_kb)
+        return states_bot.OPD
+
+    return states_bot.REMOVE_FLOWER
+
+
+async def handler_opd(update, context):
+    '''Обработчик согласия с ОПД'''
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "accept":
+        await edit_message(query, "Ввод имени", None)
+        return states_bot.NAME
+
+    elif query.data == "decline":
+        await edit_message(query, "Главное меню", main_menu_kb)
+        return states_bot.MAIN_MENU
+
+    return states_bot.OPD
+
+
+async def handler_name(update, context):
+    '''Обработчик ввода имени'''
+    context.user_data['name'] = update.message.text
+    await update.message.reply_text("Ввод номера телефона")
+    return states_bot.PHONE
+
+
+async def handler_phone(update, context):
+    '''Обработчик ввода телефона'''
+    context.user_data['phone'] = update.message.text
+    await update.message.reply_text("Ввод адреса")
+    return states_bot.ADDRESS
+
+
+async def handler_address(update, context):
+    '''Обработчик ввода адреса'''
+    context.user_data['address'] = update.message.text
+    await update.message.reply_text(
+        "Выбор даты",
+        reply_markup=delivery_date_kb
+    )
+    return states_bot.DATE
+
+
+async def handler_date(update, context):
+    '''Обработчик выбора даты'''
+    query = update.callback_query
+    await query.answer()
+
+    context.user_data['date'] = query.data
+    await edit_message(query, "Выбор времени", delivery_time_kb)
+    return states_bot.TIME
+
+
+async def handler_time(update, context):
+    '''Обработчик выбора времени'''
+    query = update.callback_query
+    await query.answer()
+
+    context.user_data['time'] = query.data
+
+    # Формирование данных заказа
+    removed_flower = context.user_data.get('removed_flower', 'не удалялись')
+
+    order_summary = f"""
+Ваш заказ:
+Имя: {context.user_data.get('name', 'Не указано')}
+Телефон: {context.user_data.get('phone', 'Не указан')}
+Адрес: {context.user_data.get('address', 'Не указан')}
+Дата: {context.user_data.get('date', 'Не указана')}
+Время: {context.user_data.get('time', 'Не указано')}
+Удаленные цветы: {removed_flower}
+Цена: 1500 руб.
+"""
+    await edit_message(query, order_summary, confirm_order_kb)
+    return states_bot.CONFIRM_ORDER
+
+
+async def handler_confirm_order(update, context):
+    '''Обработчик подтверждения заказа'''
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "confirm_order":
+        await edit_message(query, "Заказ подтвержден!", None)
+
+        # TODO: Тут отправка данных курьеру
+
+        return states_bot.COMPLETE_ORDER
+
+    elif query.data == "cancel_order":
+        await edit_message(query, "Главное меню", main_menu_kb)
+        return states_bot.MAIN_MENU
+
+    return states_bot.CONFIRM_ORDER
+
+
+async def handler_other_event(update, context):
+    '''Обработчик другого события'''
+    context.user_data['event'] = update.message.text
+    await update.message.reply_text(
+        "Выберите оттенок:",
+        reply_markup=shade_menu_kb
+    )
+    return states_bot.SHADE_MENU
--- ptb/handlers/conversation_handlers.py
+++ ptb/handlers/conversation_handlers.py
@@ -2,6 +2,8 @@ from telegram.ext import (
     CommandHandler,
     CallbackQueryHandler,
     ConversationHandler,
+    MessageHandler,
+    filters
 )
 from . import (
     callback_handlers,
@@ -13,7 +15,44 @@ conversation_handler = ConversationHandler(
     entry_points=[CommandHandler("start", cmd_handlers.start)],
     states={
         states_bot.MAIN_MENU: [
-            CallbackQueryHandler(callback_handlers.)
-        ]
-    }
+            CallbackQueryHandler(callback_handlers.handler_main_menu)
+        ],
+        states_bot.OTHER_EVENT: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, callback_handlers.handler_other_event)
+        ],
+        states_bot.SHADE_MENU: [
+            CallbackQueryHandler(callback_handlers.handler_shade_menu)
+        ],
+        states_bot.PRICE_MENU: [
+            CallbackQueryHandler(callback_handlers.handler_price_menu)
+        ],
+        states_bot.FLOWERS: [
+            CallbackQueryHandler(callback_handlers.handler_flowers)
+        ],
+        states_bot.REMOVE_FLOWER: [
+            CallbackQueryHandler(callback_handlers.handler_remove_flower)
+        ],
+        states_bot.OPD: [
+            CallbackQueryHandler(callback_handlers.handler_opd)
+        ],
+        states_bot.NAME: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, callback_handlers.handler_name)
+        ],
+        states_bot.PHONE: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, callback_handlers.handler_phone)
+        ],
+        states_bot.ADDRESS: [
+            MessageHandler(filters.TEXT & ~filters.COMMAND, callback_handlers.handler_address)
+        ],
+        states_bot.DATE: [
+            CallbackQueryHandler(callback_handlers.handler_date)
+        ],
+        states_bot.TIME: [
+            CallbackQueryHandler(callback_handlers.handler_time)
+        ],
+        states_bot.CONFIRM_ORDER: [
+            CallbackQueryHandler(callback_handlers.handler_confirm_order)
+        ],
+    },
+    fallbacks=[CommandHandler("start", cmd_handlers.start)],
 )
