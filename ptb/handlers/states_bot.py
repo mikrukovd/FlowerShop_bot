d9a6a521dde112d4a7d82101b5be423d7dcaf170
mikrukovd <z123456689z@gmail.com>
2025-09-25 08:51:23 +0300

    Добавил состояния без консультации

--- ptb/handlers/states_bot.py
+++ ptb/handlers/states_bot.py
@@ -1,3 +1,15 @@
 (
     MAIN_MENU,
-) = range(1)
+    OTHER_EVENT,
+    SHADE_MENU,
+    PRICE_MENU,
+    FLOWERS,
+    OPD,
+    NAME,
+    PHONE,
+    ADDRESS,
+    DATE,
+    TIME,
+    CONFIRM_ORDER,
+    COMPLETE_ORDER
+) = range(13)
