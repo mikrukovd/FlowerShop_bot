01d174809ed65b8e1209f62d3a015b4240bf1a0f
mikrukovd <z123456689z@gmail.com>
2025-09-26 21:55:20 +0300

    Обновил текст главного меню

--- ptb/handlers/cmd_handlers.py
+++ ptb/handlers/cmd_handlers.py
@@ -3,11 +3,16 @@ from . import states_bot
 
 
 async def start(update, context):
-    text = "Главное меню"
+    text = '''✨ *Превратим эмоции в цветы!*
+
+Каждый букет — это история. Для какого момента создадим вашу?
+• Готовые поводы ниже
+• Или свой особенный случай'''
 
     await update.message.reply_text(
         text,
-        reply_markup=keyboard.main_menu_kb
+        reply_markup=keyboard.main_menu_kb,
+        parse_mode='Markdown'
     )
 
     return states_bot.MAIN_MENU
